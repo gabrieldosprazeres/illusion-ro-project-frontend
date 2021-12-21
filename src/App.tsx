78d710667d72d9aa8ed24cf33bd06a301f049695
Miqueias <miqs_carvalho@hotmail.com>
2021-12-21 17:09:15 -0300

    feat: create component ProgessBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import ProgressBar from "./progressBar";
 
 function App() {
+  const [count, setCount] = useState<number>(0);
+
+  const handleCountPlus = () => {
+    if (count + 5 >= 500) {
+      setCount(500);
+      console.log(500);
+    } else {
+      console.log(count + 5);
+      setCount(count + 5);
+    }
+  };
+
+  const handleCountLess = () => {
+    if (count - 5 <= 0) {
+      setCount(0);
+      console.log(0);
+    } else {
+      setCount(count - 5);
+      console.log(count - 5);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ width: "500px" }}>
+          <ProgressBar currentValue={count} maxValue={500} />
+        </div>
+        <button onClick={handleCountPlus}> + </button>
+        <button onClick={handleCountLess}> - </button>
       </header>
     </div>
   );
