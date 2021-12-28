0f9c3278cc674f319a0af1ed8073d674864cb92a
Miqueias <miqs_carvalho@hotmail.com>
2021-12-28 19:09:10 -0300

    feat:creating login route

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,6 +2,7 @@ import { Container } from "./styles";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import axios from "axios";
 
 export const Login = () => {
   const formSchema = yup.object().shape({
@@ -23,17 +24,34 @@ export const Login = () => {
   }
 
   const onSubmitFunction = (data: IData) => {
-    console.log("passou");
     console.log(data);
+    axios
+      .post("https://illusion-ro.herokuapp.com/api/login", data)
+      .then((resp) => {
+        console.log(resp.data);
+        localStorage.setItem(
+          "@Illusiontoken",
+          JSON.stringify(resp.data.access_token)
+        );
+      })
+      .catch((e) => console.log(e));
   };
 
   return (
     <Container>
       <form onSubmit={handleSubmit(onSubmitFunction)}>
-        <input type="text" placeholder="username" {...register("username")} />
-        {errors.username && <p>{errors.username.message}</p>}
-        <input type="text" placeholder="password" {...register("password")} />
-        {errors.password && <p>{errors.password.message}</p>}
+        <div>
+          <input type="text" placeholder="username" {...register("username")} />
+          {errors.username && <p>{errors.username.message}</p>}
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="password"
+            {...register("password")}
+          />
+          {errors.password && <p>{errors.password.message}</p>}
+        </div>
         <button type="submit">Login</button>
       </form>
     </Container>
