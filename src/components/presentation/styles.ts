0d21bb3bcbd5fdcbd96a57fd5078a2fab527f918
Guilherme Job <guilhermejob047@gmail.com>
2021-12-29 14:42:56 -0300

    fix: remove margin: 0 auto from global styles

--- src/components/presentation/styles.ts
+++ src/components/presentation/styles.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import BackgroundPresentation from "../../Assets/FrontEnd IllusionRO/SEPARADOS/backgroundPresentation.png";
 
 export const ContainerPresentation = styled.section`
+  margin: 0 auto;
   display: flex;
   justify-content: center;
   width: 90vw;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,7 +3,7 @@ import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
 *{
-    margin: 0 auto;
+    margin: 0;
     padding: 0;
     outline: none;
     box-sizing: border-box;
