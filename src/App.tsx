7f22610a9fa3347e762b57daf6fc760d275f7bfc
ItaloSilva <iss.domingoss@gmail.com>
2021-12-31 12:04:20 -0300

    feat: version v1 site server ragnarok

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import GlobalStyle from "./styles/global";
 import { Routes } from "./routes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="App">
+      <ToastContainer />
       <GlobalStyle />
-
       <Routes />
     </div>
   );
